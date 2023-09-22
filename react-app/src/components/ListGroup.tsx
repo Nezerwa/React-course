8239c8363b3addf6ea1a099cfb50602753a1bdff
Nezerwa <eligrand2000@gmail.com>
2023-09-22 18:05:55 +0200

    deal with states

--- react-app/src/components/ListGroup.tsx
+++ react-app/src/components/ListGroup.tsx
@@ -1,5 +1,6 @@
 function ListGroup() {
   let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
+  let selectedIndex = 1;
   return (
     <div>
       <h1 className="font-bold text-3xl">List</h1>
@@ -7,7 +8,9 @@ function ListGroup() {
       <ul className="w-96">
         {items.map((item, index) => (
           <li
-            className="border p-4 bg-gray-100"
+            className={
+              selectedIndex === index ? "bg-blue-500" : "border p-4 bg-gray-100"
+            }
             key={item}
             onClick={() => console.log(item, index)}
           >
