56e6d48864f60c47f7d4e075a3cc66b6857ceeb9
Nezerwa <eligrand2000@gmail.com>
2023-09-22 16:35:06 +0200

    use ternary in react

--- react-app/src/components/ListGroup.tsx
+++ react-app/src/components/ListGroup.tsx
@@ -1,22 +1,17 @@
 function ListGroup() {
-  const name = "Eligrand";
+  let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
+const message = items.length === 0 ? <p>No items Found</p> : null;
   return (
-    <ul className="w-96">
-      <li className="w-full border-b-2 border-neutral-100 border-opacity-100 py-4 dark:border-opacity-50">
-        An item
-      </li>
-      <li className="w-full border-b-2 border-neutral-100 border-opacity-100 py-4 dark:border-opacity-50">
-        A second item
-      </li>
-      <li className="w-full border-b-2 border-neutral-100 border-opacity-100 py-4 dark:border-opacity-50">
-        A third item
-      </li>
-      <li className="w-full border-b-2 border-neutral-100 border-opacity-100 py-4 dark:border-opacity-50">
-        A fourth item
-      </li>
-      <li className="w-full py-4">And a fifth one</li>
-    </ul>
+    <div>
+      <h1 className="font-bold text-3xl">List</h1>
+      {message}
+      <ul className="w-96">
+        {items.map((item) => (
+          <li key={item}>{item}</li>
+        ))}
+      </ul>
+    </div>
   );
 }
 
-export default ListGroup ;
+export default ListGroup;
