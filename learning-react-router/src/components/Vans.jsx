ca53632a1eb4cfe4a5c8a2ddbcd35fc4b05aa94d
Nezerwa <eligrand2000@gmail.com>
2023-10-05 09:22:16 +0200

    Render cards using milage api

--- learning-react-router/src/components/Vans.jsx
+++ learning-react-router/src/components/Vans.jsx
@@ -1,13 +1,29 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
-function Vans(){
-    useEffect(()=>{
-        fetch("/api/vans").then((response)=> response.json()).then((response)=> console.log(response));
-    })
-    return (
-      <div>
-        <h1>Vans page goes here 🚐</h1>
+function Vans() {
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    console.log("log");
+    fetch("/api/vans")
+      .then((response) => response.json())
+      .then((response) => setData(response.vans));
+  }, []);
+  console.log(data);
+  const vansElement = data.map((van) => (
+    <div key={van.id} className="van-tile">
+      <img src={van.imageUrl} alt="van" />
+      <div className="van-info">
+        <h3>{van.name}</h3>
+        <p>{van.price}</p>
+        <i className={`van-type ${van.type} selected`}>{van.type}</i>
       </div>
-    );
+    </div>
+  ));
+  return (
+    <div className="van-list-container">
+      <h1>Explore our van options</h1>
+      <div className="van-list">{vansElement}</div>
+    </div>
+  );
 }
-export default Vans
+export default Vans;
