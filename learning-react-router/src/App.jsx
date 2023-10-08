0d0f13c5435ade8d1bc9565113abfba4153e3a2c
Nezerwa <eligrand2000@gmail.com>
2023-10-08 16:13:33 +0200

    Update the millage server

--- learning-react-router/src/App.jsx
+++ learning-react-router/src/App.jsx
@@ -3,11 +3,17 @@ import Home from "./components/Home";
 import About from "./components/About";
 import Vans from "./components/Vans";
 import Details from "./components/vanDetail";
-import Layout from "./components/layout";
+import Layout from "./components/Layout";
 import Dashboard from "./components/host/Dashboard";
 import Reviews from "./components/host/Reviews";
 import Income from "./components/host/Income";
 import HostLayout from "./components/HostLayout";
+import HostVans from "./components/host/HostVan";
+import HostVanDetail from "./components/host/hostVanDetail";
+import HostVanInfo from "./components/host/hostVaninfo";
+import HostVanPricing from "./components/host/hostVanpricing";
+import HostVanPhotos from "./components/host/hostvanphotos";
+
 function App() {
   return (
     <BrowserRouter>
@@ -21,6 +27,12 @@ function App() {
             <Route index element={<Dashboard />} />
             <Route path="reviews" element={<Reviews />} />
             <Route path="income" element={<Income />} />
+            <Route path="vans" element={<HostVans />} />
+          </Route>
+          <Route path="vans/:id" element={<HostVanDetail />}>
+            <Route index element={<HostVanInfo />} />
+            <Route path="pricing" element={<HostVanPricing />} />
+            <Route path="photos" element={<HostVanPhotos />} />
           </Route>
         </Route>
       </Routes>
--- learning-react-router/src/components/HostLayout.jsx
+++ learning-react-router/src/components/HostLayout.jsx
@@ -1,14 +1,46 @@
+import React from "react";
 import { NavLink, Outlet } from "react-router-dom";
-function HostLayout() {
+
+export default function HostLayout() {
+  const activeStyles = {
+    fontWeight: "bold",
+    textDecoration: "underline",
+    color: "#161616",
+  };
+
   return (
-    <div>
+    <>
       <nav className="host-nav">
-        <NavLink to="/host">Dashboard</NavLink>
-        <NavLink to="/host/income">Income</NavLink>
-        <NavLink to="/host/reviews">Reviews</NavLink>
+        <NavLink
+          to="."
+          end
+          style={({ isActive }) => (isActive ? activeStyles : null)}
+        >
+          Dashboard
+        </NavLink>
+
+        <NavLink
+          to="income"
+          style={({ isActive }) => (isActive ? activeStyles : null)}
+        >
+          Income
+        </NavLink>
+
+        <NavLink
+          to="vans"
+          style={({ isActive }) => (isActive ? activeStyles : null)}
+        >
+          Vans
+        </NavLink>
+
+        <NavLink
+          to="reviews"
+          style={({ isActive }) => (isActive ? activeStyles : null)}
+        >
+          Reviews
+        </NavLink>
       </nav>
       <Outlet />
-    </div>
+    </>
   );
 }
-export default HostLayout;
